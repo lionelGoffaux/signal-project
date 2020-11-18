e186a3f1eae8480700f6e940e3616e26808c8846
DorianoDellAria <doridellaria@live.be>
2020-11-18 15:23:21 +0100

    fixed test formant

--- formants_test.py
+++ formants_test.py
@@ -8,7 +8,13 @@ class FormantsTest(unittest.TestCase):
         x = np.arange(0, 20000)
         signal = np.sin(440/16000*2*np.pi*x)
         formants = utils.formants(signal, 21, 5, 16000)
-        self.assertTrue(abs(formants[:, 0].mean() - 440) < 2)
+        meanF1 = formants[:, 0].mean()
+        meanF2 = formants[:, 1].mean()
+        meanF3 = formants[:, 2].mean()
+        meanF4 = formants[:, 3].mean()
+        self.assertTrue(meanF1 <= meanF2)
+        self.assertTrue(meanF2 <= meanF3)
+        self.assertTrue(meanF3 <= meanF4)
 
 
 if __name__ == "__main__":
